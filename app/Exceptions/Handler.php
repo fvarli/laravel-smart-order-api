9824a703515336f5878ab5d762ba37cc0f9926ce
Ferzender Varli <fvarlii@gmail.com>
2025-03-03 20:06:29 +0300

    refactor: refactor handled error response messages, API-only enforced, JSON errors fixed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,16 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +35,59 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * @param $request
+     * @param Throwable $exception
+     * @return Response|JsonResponse|\Symfony\Component\HttpFoundation\Response|RedirectResponse
+     * @throws Throwable
+     */
+    public function render($request, Throwable $exception): Response|JsonResponse|\Symfony\Component\HttpFoundation\Response|RedirectResponse
+    {
+        // 404 - Model Not Found
+        if ($exception instanceof ModelNotFoundException || $exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found.',
+            ], 404);
+        }
+
+        // 429 - Too Many Requests
+        if ($exception instanceof ThrottleRequestsException) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Too many requests. Please try again later.',
+            ], 429);
+        }
+
+        // 401 - Authentication Failure
+        if ($exception instanceof AuthenticationException) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthenticated.',
+            ], 401);
+        }
+
+        // 422 - Validation Error
+        if ($exception instanceof ValidationException) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation error.',
+                'errors' => $exception->errors(),
+            ], 422);
+        }
+
+        // 405 - Method Not Allowed
+        if ($request->is('api/*')) {
+            if ($exception instanceof MethodNotAllowedHttpException) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Invalid request method. Please check API documentation.'
+                ], 405);
+            }
+        }
+        // Default: Return Parent Handler
+        return parent::render($request, $exception);
     }
 }
